2e51a4c5a127316ae0481fdd8e36d77e439dd2c0
NAZIM AMEER <nazimameerpp@gmail.com>
2023-09-28 13:57:24 +0530

    schema for updateMutation

--- schema.js
+++ schema.js
@@ -32,11 +32,17 @@ export const typeDefs = `#graphql
 
  type Mutation {
    addGame(game: AddGameInput): Game
-    deleteGame(id: ID!): [Game]
+   deleteGame(id: ID!): [Game]
+   updateMutation(id: ID!, edits: EditGameInput!): Game
   }
 
   input AddGameInput {
    title: String!,
    platform: [String!]!
+  }
+
+  input EditGameInput {
+   title: String,
+   platform: [String!]
   }
 `
